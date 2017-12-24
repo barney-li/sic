175c69cfbdd367441eff24f81673816caf2c063b
barney-li <barney.li@outlook.com>
2017-12-24 22:55:02 +0800

    log accuracy for tensorboard

--- src/train.py
+++ src/train.py
@@ -15,9 +15,13 @@ def get_sic_training_data(path='../data/train.json'):
 
 def model():
     with tf.name_scope('reshape'):
-        x_in = tf.placeholder(tf.float32)
-        y_in = tf.placeholder(tf.float32)
-        learning_rate_in = tf.placeholder(tf.float32)
+        trn_acc_labels_in = tf.placeholder(tf.float32, name='trn_acc_labels_in')
+        trn_acc_logits_in = tf.placeholder(tf.float32, name='trn_acc_logits_in')
+        tst_acc_labels_in = tf.placeholder(tf.float32, name='tst_acc_labels_in')
+        tst_acc_logits_in = tf.placeholder(tf.float32, name='tst_acc_logits_in')
+        x_in = tf.placeholder(tf.float32, name='x_in')
+        y_in = tf.placeholder(tf.float32, name='y_in')
+        learning_rate_in = tf.placeholder(tf.float32, name='learning_rate_in')
         x = tf.image.resize_images(tf.reshape(x_in, [-1, 75, 75, 2]), [256, 256], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
         y = tf.reshape(y_in, [-1, 2])
     with tf.name_scope('resnet'):
@@ -29,9 +33,21 @@ def model():
         correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
         correct_prediction = tf.cast(correct_prediction, tf.float32)
         accuracy = tf.reduce_mean(correct_prediction)
+    with tf.name_scope('trn_acc'):
+        trn_acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(trn_acc_labels_in, 1), tf.argmax(trn_acc_logits_in, 1)), tf.float32))
+        tf.summary.scalar("trn_acc", trn_acc)
+    with tf.name_scope('tst_acc'):
+        tst_acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(tst_acc_labels_in, 1), tf.argmax(tst_acc_logits_in, 1)), tf.float32))
+        tf.summary.scalar("tst_acc", tst_acc)
     with tf.name_scope('optimizer'):
         optimizer = tf.train.AdamOptimizer(learning_rate_in).minimize(cost)
-    return x_in, y_in, y_, cost, accuracy, optimizer, learning_rate_in
+    summ = tf.summary.merge_all()
+    ret = {'x_in': x_in, 'y_in': y_in, 'y_': y_, 'cost': cost, 'accuracy': accuracy, 'optimizer': optimizer,
+           'learning_rate_in': learning_rate_in, 'trn_acc_labels_in': trn_acc_labels_in,
+           'trn_acc_logits_in': trn_acc_logits_in, 'trn_acc': trn_acc,
+           'tst_acc_labels_in': tst_acc_labels_in, 'tst_acc_logits_in': tst_acc_logits_in,
+           'tst_acc': tst_acc, 'summ': summ}
+    return ret
 
 
 def ia(start_index, end_index, output):
@@ -88,10 +104,24 @@ def randomize(arr, n):
     return arr
 
 
-def train(ia_output, batch_size, epoch_size, fold_size, learning_rate, ckpt):
+def train(ia_output, batch_size, epoch_size, fold_size, learning_rate, ckpt, logdir):
     if ckpt is None:
         print('training')
-        x_in, y_in, y_, cost, accuracy, optimizer, learning_rate_in = model()
+        md = model()
+        x_in = md['x_in']
+        y_in = md['y_in']
+        y_ = md['y_']
+        cost = md['cost']
+        accuracy = md['accuracy']
+        optimizer = md['optimizer']
+        learning_rate_in = md['learning_rate_in']
+        trn_acc_labels_in = md['trn_acc_labels_in']
+        trn_acc_logits_in = md['trn_acc_logits_in']
+        trn_acc = md['trn_acc']
+        tst_acc_labels_in = md['tst_acc_labels_in']
+        tst_acc_logits_in = md['tst_acc_logits_in']
+        tst_acc = md['tst_acc']
+        summ = md['summ']
     else:
         print('training from ckpt {}'.format(ckpt))
 
@@ -107,6 +137,13 @@ def train(ia_output, batch_size, epoch_size, fold_size, learning_rate, ckpt):
             tf.add_to_collection('optimizer', optimizer)
             tf.add_to_collection('learning_rate_in', learning_rate_in)
             tf.add_to_collection('accuracy', accuracy)
+            tf.add_to_collection('trn_acc', trn_acc)
+            tf.add_to_collection('trn_acc_labels_in', trn_acc_labels_in)
+            tf.add_to_collection('trn_acc_logits_in', trn_acc_logits_in)
+            tf.add_to_collection('tst_acc', tst_acc)
+            tf.add_to_collection('tst_acc_labels_in', tst_acc_labels_in)
+            tf.add_to_collection('tst_acc_logits_in', tst_acc_logits_in)
+
         else:
             print('loading ckpt {}'.format(ckpt))
             saver = tf.train.import_meta_graph('../models/{}.ckpt.meta'.format(ckpt))
@@ -119,7 +156,15 @@ def train(ia_output, batch_size, epoch_size, fold_size, learning_rate, ckpt):
             optimizer = tf.get_collection('optimizer')[0]
             learning_rate_in = tf.get_collection('learning_rate_in')[0]
             accuracy = tf.get_collection('accuracy')[0]
+            trn_acc = tf.get_collection('trn_acc')[0]
+            trn_acc_labels_in = tf.get_collection('trn_acc_labels_in')[0]
+            trn_acc_logits_in = tf.get_collection('trn_acc_logits_in')[0]
+            tst_acc = tf.get_collection('tst_acc')[0]
+            tst_acc_labels_in = tf.get_collection('tst_acc_labels_in')[0]
+            tst_acc_logits_in = tf.get_collection('tst_acc_logits_in')[0]
 
+        writer = tf.summary.FileWriter(logdir)
+        writer.add_graph(sess.graph)
         # epoch
         if epoch_size is None:
             epoch_size = int(train_x.shape[0] / 64)
@@ -131,17 +176,25 @@ def train(ia_output, batch_size, epoch_size, fold_size, learning_rate, ckpt):
                 sess.run(optimizer, feed_dict={x_in: x_batch, y_in: y_batch, learning_rate_in: learning_rate})
                 # print('epoch {} batch {}'.format(i, j))
             # print accuracy after each epoch
-            train_accuracy = 0
+            trn_acc_labels = None
+            trn_acc_logits = None
             for batch in range(epoch_size):
                 x_batch = train_x[batch * batch_size: (batch + 1) * batch_size]
                 y_batch = train_y[batch * batch_size: (batch + 1) * batch_size]
-                train_accuracy += accuracy.eval(session=sess,
-                                               feed_dict={
-                                                   x_in: x_batch, y_in: y_batch
-                                               })
-            train_accuracy /= int(epoch_size)
-            print('epoch {}, training accuracy {}'.format(epoch, train_accuracy))
-            print('test accuracy {}'.format(accuracy.eval(session=sess, feed_dict={x_in: test_x, y_in: test_y})))
+                res = y_.eval(session=sess, feed_dict={x_in:x_batch})
+                if trn_acc_labels is None:
+                    trn_acc_labels = y_batch
+                    trn_acc_logits = res
+                else:
+                    np.concatenate((trn_acc_labels, y_batch))
+                    np.concatenate((trn_acc_logits, res))
+            tst_acc_logits = y_.eval(session=sess, feed_dict={x_in:test_x})
+            [trn_acc_out, tst_acc_out, summ_out] = sess.run([trn_acc, tst_acc, summ], feed_dict={trn_acc_labels_in:trn_acc_labels,
+                                                                       trn_acc_logits_in:trn_acc_logits,
+                                                                       tst_acc_labels_in:test_y,
+                                                                       tst_acc_logits_in:tst_acc_logits})
+            print('epoch {} train accuracy {} test accuracy {}'.format(epoch, batch, trn_acc_out, tst_acc_out))
+            writer.add_summary(summ_out, epoch)
             saver = tf.train.Saver()
             print('saving ckpt...')
             saved_path = saver.save(sess, '../models/{}.ckpt'.format(epoch))
@@ -190,9 +243,10 @@ if __name__ == '__main__':
     parser.add_argument('--fold_size', type=int, default=1000)
     parser.add_argument('--learning_rate', type=float, default=1e-4)
     parser.add_argument('--ckpt', type=str)
+    parser.add_argument('--logdir', type=str, default='../logs/default')
     args = parser.parse_args()
     if args.mode == 'train':
-        train(args.ia_output, args.batch_size, args.epoch_size, args.fold_size, args.learning_rate, args.ckpt)
+        train(args.ia_output, args.batch_size, args.epoch_size, args.fold_size, args.learning_rate, args.ckpt, args.logdir)
     elif args.mode == 'ia':
         ia(args.ia_start, args.ia_end, args.ia_output)
     elif args.mode == 'combine_ia':
