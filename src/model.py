a4af8b5cf5b95c999f9e73e151c8c29d084162b7
barney-li <barney.li@outlook.com>
2017-12-31 21:32:35 +0800

    using new way to retrieve tensor and operation from graph; the training seems to be fine now even if is_training is used, although it requires lots of epochs before cost starts to converge in my laptop

--- src/model.py
+++ src/model.py
@@ -3,45 +3,25 @@ import resnet_model
 def model():
     with tf.variable_scope('input'):
         is_training = tf.placeholder(tf.bool, name='is_training')
-        trn_acc_labels_in = tf.placeholder(tf.float32, name='trn_acc_labels_in')
-        trn_acc_logits_in = tf.placeholder(tf.float32, name='trn_acc_logits_in')
-        tst_acc_labels_in = tf.placeholder(tf.float32, name='tst_acc_labels_in')
-        tst_acc_logits_in = tf.placeholder(tf.float32, name='tst_acc_logits_in')
         x_in = tf.placeholder(tf.float32, name='x_in')
         y_in = tf.placeholder(tf.float32, name='y_in')
-        learning_rate_in = tf.placeholder(tf.float32, name='learning_rate_in')
-    with tf.name_scope('reshape'):
-        x = tf.image.resize_images(tf.reshape(x_in, [-1, 75, 75, 2]), [224, 224], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-        y = tf.reshape(y_in, [-1, 2])
-    with tf.name_scope('resnet'):
+        learning_rate = tf.placeholder(tf.float32, name='learning_rate')
+        process = tf.placeholder(tf.string, name='process')
+        images = tf.image.resize_images(tf.reshape(x_in, [-1, 75, 75, 2]), [224, 224], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+        labels = tf.reshape(y_in, [-1, 2])
+    with tf.variable_scope('resnet'):
         y_generator = resnet_model.imagenet_resnet_v2(18, 2, 'channels_last')
-        y_ = y_generator(x, is_training)
-    with tf.name_scope('cost'):
-        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_, labels=y))
-    with tf.name_scope('trn_cost'):
-        trn_cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=trn_acc_logits_in, labels=trn_acc_labels_in))
-        tf.summary.scalar('trn_cost', trn_cost)
-    with tf.name_scope('tst_cost'):
-        tst_cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=tst_acc_logits_in, labels=tst_acc_labels_in))
-        tf.summary.scalar('tst_cost', tst_cost)
-    with tf.name_scope('accuracy'):
-        correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+        tmp = y_generator(images, is_training)
+    with tf.variable_scope('output'):
+        logits = tf.identity(tmp, 'logits')
+        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels), name='cost')
+        correct_prediction = tf.equal(tf.argmax(labels, 1), tf.argmax(logits, 1))
         correct_prediction = tf.cast(correct_prediction, tf.float32)
-        accuracy = tf.reduce_mean(correct_prediction)
-    with tf.name_scope('trn_acc'):
-        trn_acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(trn_acc_labels_in, 1), tf.argmax(trn_acc_logits_in, 1)), tf.float32))
-        tf.summary.scalar("trn_acc", trn_acc)
-    with tf.name_scope('tst_acc'):
-        tst_acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(tst_acc_labels_in, 1), tf.argmax(tst_acc_logits_in, 1)), tf.float32))
-        tf.summary.scalar("tst_acc", tst_acc)
-    with tf.name_scope('optimizer'):
+        accuracy = tf.reduce_mean(correct_prediction, name='accuracy')
+        probability_ = tf.nn.softmax(logits, name='probability')
+        process = tf.Print(process, [process])
+        tf.summary.scalar('{} cost'.format(process), cost)
+        tf.summary.scalar('{} accuracy'.format(process), accuracy)
         with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
-            optimizer = tf.train.AdamOptimizer(learning_rate_in).minimize(cost)
-    summ = tf.summary.merge_all()
-    ret = {'x_in': x_in, 'y_in': y_in, 'y_': y_, 'cost': cost, 'accuracy': accuracy, 'optimizer': optimizer,
-           'learning_rate_in': learning_rate_in, 'trn_acc_labels_in': trn_acc_labels_in,
-           'trn_acc_logits_in': trn_acc_logits_in, 'trn_acc': trn_acc,
-           'tst_acc_labels_in': tst_acc_labels_in, 'tst_acc_logits_in': tst_acc_logits_in,
-           'tst_acc': tst_acc, 'trn_cost': trn_cost, 'tst_cost': tst_cost, 'summ': summ,
-           'is_training': is_training}
-    return ret
+            optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost, name='optimizer')
+    return optimizer
--- src/train.py
+++ src/train.py
@@ -8,29 +8,7 @@ import model
 def train(batch_size, epoch_size, fold_size, learning_rate, ckpt, logdir, no_ia, regen_data):
     tf.reset_default_graph()
     with tf.Graph().as_default():
-        if ckpt is None:
-            print('training')
-            md = model.model()
-            x_in = md['x_in']
-            y_in = md['y_in']
-            y_ = md['y_']
-            cost = md['cost']
-            accuracy = md['accuracy']
-            optimizer = md['optimizer']
-            learning_rate_in = md['learning_rate_in']
-            trn_acc_labels_in = md['trn_acc_labels_in']
-            trn_acc_logits_in = md['trn_acc_logits_in']
-            trn_acc = md['trn_acc']
-            tst_acc_labels_in = md['tst_acc_labels_in']
-            tst_acc_logits_in = md['tst_acc_logits_in']
-            tst_acc = md['tst_acc']
-            trn_cost = md['trn_cost']
-            tst_cost = md['tst_cost']
-            summ = md['summ']
-            is_training = md['is_training']
-        else:
-            print('training from ckpt {}'.format(ckpt))
-
+        print('training')
         total_x, total_y = utils.get_train_data(regen_data=regen_data, no_ia=no_ia)
         train_x = total_x[200:]
         train_y = total_y[200:]
@@ -39,48 +17,26 @@ def train(batch_size, epoch_size, fold_size, learning_rate, ckpt, logdir, no_ia,
 
         with tf.Session() as sess:
             if ckpt is None:
+                model.model()
                 sess.run(tf.global_variables_initializer())
-                tf.add_to_collection('cost', cost)
-                tf.add_to_collection('y_', y_)
-                tf.add_to_collection('x_in', x_in)
-                tf.add_to_collection('y_in', y_in)
-                tf.add_to_collection('optimizer', optimizer)
-                tf.add_to_collection('learning_rate_in', learning_rate_in)
-                tf.add_to_collection('accuracy', accuracy)
-                tf.add_to_collection('trn_acc', trn_acc)
-                tf.add_to_collection('trn_acc_labels_in', trn_acc_labels_in)
-                tf.add_to_collection('trn_acc_logits_in', trn_acc_logits_in)
-                tf.add_to_collection('tst_acc', tst_acc)
-                tf.add_to_collection('tst_acc_labels_in', tst_acc_labels_in)
-                tf.add_to_collection('tst_acc_logits_in', tst_acc_logits_in)
-                tf.add_to_collection('trn_cost', trn_cost)
-                tf.add_to_collection('tst_cost', tst_cost)
-                tf.add_to_collection('is_training', is_training)
-
             else:
                 print('loading ckpt {}'.format(ckpt))
                 saver = tf.train.import_meta_graph('../models/{}.ckpt.meta'.format(ckpt))
                 saver.restore(sess, '../models/{}.ckpt'.format(ckpt))
                 print('ckpt loaded')
-                cost = tf.get_collection('cost')[0]
-                y_ = tf.get_collection('y_')[0]
-                x_in = tf.get_collection('x_in')[0]
-                y_in = tf.get_collection('y_in')[0]
-                optimizer = tf.get_collection('optimizer')[0]
-                learning_rate_in = tf.get_collection('learning_rate_in')[0]
-                accuracy = tf.get_collection('accuracy')[0]
-                trn_acc = tf.get_collection('trn_acc')[0]
-                trn_acc_labels_in = tf.get_collection('trn_acc_labels_in')[0]
-                trn_acc_logits_in = tf.get_collection('trn_acc_logits_in')[0]
-                tst_acc = tf.get_collection('tst_acc')[0]
-                tst_acc_labels_in = tf.get_collection('tst_acc_labels_in')[0]
-                tst_acc_logits_in = tf.get_collection('tst_acc_logits_in')[0]
-                trn_cost = tf.get_collection('trn_cost')[0]
-                tst_cost = tf.get_collection('tst_cost')[0]
-                is_training = tf.get_collection('is_training')[0]
 
             graph = sess.graph
-            is_training_tensor = graph.get_tensor_by_name('input/is_training:0')
+            is_training_t = graph.get_tensor_by_name('input/is_training:0')
+            optimizer_t = graph.get_operation_by_name('output/optimizer')
+            x_in_t = graph.get_tensor_by_name('input/x_in:0')
+            y_in_t = graph.get_tensor_by_name('input/y_in:0')
+            process_t = graph.get_tensor_by_name('input/process:0')
+            logits_t = graph.get_tensor_by_name('output/logits:0')
+            learning_rate_t = graph.get_tensor_by_name('input/learning_rate:0')
+            accuracy_t = graph.get_tensor_by_name('output/accuracy:0')
+            probability_t = graph.get_tensor_by_name('output/probability:0')
+            cost_t = graph.get_tensor_by_name('output/cost:0')
+            summ_t = tf.summary.merge_all()
 
             writer = tf.summary.FileWriter(logdir)
             writer.add_graph(sess.graph)
@@ -92,32 +48,29 @@ def train(batch_size, epoch_size, fold_size, learning_rate, ckpt, logdir, no_ia,
                 for batch in range(epoch_size):
                     x_batch = train_x[batch * batch_size: (batch + 1) * batch_size]
                     y_batch = train_y[batch * batch_size: (batch + 1) * batch_size]
-                    sess.run(optimizer, feed_dict={x_in: x_batch, y_in: y_batch, learning_rate_in: learning_rate, is_training: False})
+                    sess.run(optimizer_t, feed_dict={x_in_t: x_batch, y_in_t: y_batch, learning_rate_t: learning_rate,
+                                                     is_training_t: True, process_t: 'train step'})
 
                 # print accuracy after each epoch
-                trn_acc_labels = None
-                trn_acc_logits = None
+                train_acc = []
+                train_cost = []
                 for batch in range(epoch_size):
                     x_batch = train_x[batch * batch_size: (batch + 1) * batch_size]
                     y_batch = train_y[batch * batch_size: (batch + 1) * batch_size]
-                    res = y_.eval(session=sess, feed_dict={x_in:x_batch, is_training: False})
-                    if trn_acc_labels is None:
-                        trn_acc_labels = y_batch
-                        trn_acc_logits = res
-                    else:
-                        np.concatenate((trn_acc_labels, y_batch))
-                        np.concatenate((trn_acc_logits, res))
-                tst_acc_logits = y_.eval(session=sess, feed_dict={x_in:test_x, is_training: False})
-                [trn_acc_out, tst_acc_out, trn_cost_out, tst_cost_out, summ_out] = sess.run([trn_acc, tst_acc, trn_cost, tst_cost, summ],
-                                                                feed_dict={trn_acc_labels_in:trn_acc_labels,
-                                                                           trn_acc_logits_in:trn_acc_logits,
-                                                                           tst_acc_labels_in:test_y,
-                                                                           tst_acc_logits_in:tst_acc_logits,
-                                                                           is_training: False})
+                    [tmp_acc, tmp_cost, summ] = sess.run([accuracy_t, cost_t, summ_t],
+                                                   feed_dict={x_in_t:x_batch, y_in_t:y_batch, is_training_t: False,
+                                                              process_t: 'eval train'})
+                    train_acc.append(tmp_acc)
+                    train_cost.append(tmp_cost)
+                writer.add_summary(summ, epoch)
+                [test_acc, test_cost, summ] = sess.run([accuracy_t, cost_t, summ_t],
+                                                 feed_dict={x_in_t:test_x, is_training_t: False, y_in_t:test_y,
+                                                            process_t: 'eval test'})
+
                 print('\nepoch {}'.format(epoch))
-                print('train accuracy {} train cost {}'.format(trn_acc_out, trn_cost_out))
-                print('test accuracy {} test cost {}'.format(tst_acc_out, tst_cost_out))
-                writer.add_summary(summ_out, epoch)
+                print('train accuracy {} train cost {}'.format(np.mean(train_acc), np.mean(train_cost)))
+                print('test accuracy {} test cost {}'.format(test_acc, test_cost))
+                writer.add_summary(summ, epoch)
                 saver = tf.train.Saver()
                 print('saving ckpt...')
                 saved_path = saver.save(sess, '../models/{}.ckpt'.format(epoch))
